f6489da83832702587bb62a565949fbee000e086
Myles Wilter-Heritage <myles.wilter-heritage@blackswan.com>
2017-06-06 11:34:36 +0100

    Add application

---
+++ app/src/main/java/dankmemes/myleswh/dankmemes/DankApplication.java
@@ -0,0 +1,15 @@
+package dankmemes.myleswh.dankmemes;
+
+import android.app.Application;
+
+/**
+ * Created by myleswh on 06/06/2017.
+ */
+
+public class DankApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
