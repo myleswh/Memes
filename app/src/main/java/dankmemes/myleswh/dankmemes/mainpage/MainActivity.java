e1991c31c976b385aee81b93165f46c38ff57938
Myles Wilter-Heritage <myles.wilter-heritage@blackswan.com>
2017-06-06 11:42:35 +0100

    Butter knife and add recyclerView

--- app/src/main/java/dankmemes/myleswh/dankmemes/mainpage/MainActivity.java
+++ app/src/main/java/dankmemes/myleswh/dankmemes/mainpage/MainActivity.java
@@ -1,7 +1,9 @@
-package dankmemes.myleswh.dankmemes;
+package dankmemes.myleswh.dankmemes.mainpage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import dankmemes.myleswh.dankmemes.R;
 
 public class MainActivity extends AppCompatActivity {
 
